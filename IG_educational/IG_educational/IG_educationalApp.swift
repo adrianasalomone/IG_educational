dc35494f25ee8c9e89233280bb904c42f2916e36
adrianasalomone <asalomone22@fed.idserve.net>
2022-11-21 15:35:52 +0100

    First commit

---
+++ IG_educational/IG_educational/IG_educationalApp.swift
@@ -0,0 +1,17 @@
+//
+//  IG_educationalApp.swift
+//  IG_educational
+//
+//  Created by Adriana Salomone on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct IG_educationalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
